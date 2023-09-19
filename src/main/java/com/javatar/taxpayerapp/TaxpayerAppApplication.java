207e36544894e46a0339c15366375242b60660ad
e.sadeghi <e.sadeghi@fwutech.com>
2023-09-19 02:05:18 -0400

    first commit

---
+++ src/main/java/com/javatar/taxpayerapp/TaxpayerAppApplication.java
@@ -0,0 +1,13 @@
+package com.javatar.taxpayerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxpayerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxpayerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatar/taxpayerapp/TaxpayerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatar.taxpayerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxpayerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
